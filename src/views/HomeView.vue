a70a1e517af3d149eaabd5887686b316a9ce36e0
luthfizone <luthfizonedev@gmail.com>
2025-01-18 20:53:41 +0700

    pengenalan transition

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,25 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from "vue";
+
+const showText = ref(false);
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Transition name="fade">
+      <h1 v-if="showText">Hello World</h1>
+    </Transition>
+    <button @click="showText = !showText">Toggle</button>
   </main>
 </template>
+
+<style scoped>
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+</style>
