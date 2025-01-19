2676643a82cb82aff39d6c150c94064cdfe825c8
luthfizone <luthfizonedev@gmail.com>
2025-01-19 16:31:03 +0700

    feat: add todo content ui

---
+++ src/components/TodoContent.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+
+<template>
+  <div class="card-list">
+    <div>Task 1</div>
+    <div>Task 2</div>
+    <div>Task 3</div>
+  </div>
+</template>
+
+<style scoped>
+.card-list {
+  margin-top: 1.5rem;
+  width: 50%;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.card-list div {
+  margin-top: 1rem;
+  padding: 5% 10%;
+  text-align: center;
+  cursor: pointer;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.2);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Header from "@/components/Header.vue";
 import InputUser from "@/components/InputUser.vue";
+import TodoContent from "@/components/TodoContent.vue";
 </script>
 
 <template>
   <main>
     <Header />
     <InputUser />
+    <TodoContent />
   </main>
 </template>
